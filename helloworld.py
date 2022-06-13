8bd9cdefbf7d087a0a5fd8c1a47ad5af20504319
Mark Leager <markleager92@gmail.com>
2022-06-13 19:32:08 -0400

    add helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+def main():
+    print("""
+    Two things are infinite:
+    the universe and human stupidity;
+    and I'm not sure about the universe.
+    - Christopher Walken""")
+
+
+if __name__ == "__main__":
+    main()
