4e2b6f274bfb941c84edcdaf613dc3280fdef849
Mark Leager <106631893+mleager@users.noreply.github.com>
2022-06-13 20:30:53 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,10 @@ def main():
     Two things are infinite:
     the universe and human stupidity;
     and I'm not sure about the universe.
-    - Christopher Walken""")
+    - Christopher Walken
+    From Python:""" + str(sys.version_info))
+    if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
+        raise Exception("Python version 3.6x is unsupported!")
 
 
 if __name__ == "__main__":
